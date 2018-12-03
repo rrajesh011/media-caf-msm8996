b4bbea9a6e37f1e5cff776cdfc5c0d25a78720a9
Rajesh <rrajesh8087@gmail.com>
2018-12-03 16:42:19 +0000

    initial commit

---
+++ Android.mk
@@ -0,0 +1,15 @@
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf-msm8996)
+QCOM_MEDIA_ROOT := $(call my-dir)
+
+include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
+include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
+
+include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
+
+ifeq ($(BOARD_USES_ADRENO), true)
+include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
+endif
+
+include $(QCOM_MEDIA_ROOT)/hypv-intercept/Android.mk
+
+endif
